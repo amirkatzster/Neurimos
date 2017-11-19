84b1a64333316702f63074b2b5b4c17e30c35b73
Amir Katz <amirkatzster@gmail.com>
2017-11-20 00:07:33 +0200

    ci

--- scripts/post.sh
+++ scripts/post.sh
@@ -3,18 +3,18 @@
 set -e
 
 echo 'starting to run post script'
-cd ~/www
 ## ownership
 chown root:root /usr/bin/sudo && chmod 4755 /usr/bin/sudo
+cd ~/www
 sudo chown -R bitnami:bitnami ../
 echo 'ownership changed'
 
 ## Set up node
 echo 'init'
-npm i 
+#npm i 
 echo 'build ng'
 #npm rebuild node-sass --force
-/opt/bitnami/nodejs/bin/ng build -prod 2>&1 | tee outfile
+#/opt/bitnami/nodejs/bin/ng build -prod 2>&1 | tee outfile
 
 echo 'end'
 
