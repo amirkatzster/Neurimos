d824447621928e8a49845f209644c5e81a939852
Amir Katz <amirkatzster@gmail.com>
2017-11-19 21:51:57 +0200

    better...

--- scripts/post.sh
+++ scripts/post.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
-pwd
-ls
-cd ~/www
-pwd
-ls
-
-whoami
-
-
 echo 'starting to run post script'
-
+cd ~/www
 ## ownership
 sudo chown -R bitnami:bitnami ../
 echo 'ownership changed'
 
 ## Set up node
-sudo npm -i 
-#ng build -prod
+npm i 
+ng build -aot -prod
+
 
