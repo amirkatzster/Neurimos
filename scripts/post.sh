579ab4338b6c376f2d0e7b2e24ab836c6abab87b
Amir Katz <amirkatzster@gmail.com>
2017-11-20 00:17:26 +0200

    back

--- scripts/post.sh
+++ scripts/post.sh
@@ -5,20 +5,21 @@ set -e
 echo 'starting to run post script'
 ## ownership
 whoami
-chown root:root /usr/bin/sudo && chmod 4755 /usr/bin/sudo
-chown root:root /usr/lib/sudo/sudoers.so && chmod 4755 /usr/lib/sudo/sudoers.so; chown root:root /etc/sudoers; chown root:root /etc/sudoers;
-chown root:root /etc/sudoers.d && chmod 4755 /etc/sudoers.d; chown root:root /var/lib/sudo; chown root:root /var/lib/sudo;
-chown -R root:root /etc 
+#chown root:root /usr/bin/sudo && chmod 4755 /usr/bin/sudo
+#chown root:root /usr/lib/sudo/sudoers.so && chmod 4755 /usr/lib/sudo/sudoers.so; chown root:root /etc/sudoers; chown root:root /etc/sudoers;
+#chown root:root /etc/sudoers.d && chmod 4755 /etc/sudoers.d; chown root:root /var/lib/sudo; chown root:root /var/lib/sudo;
+#chown -R root:root /etc 
+
 cd ~/www
 sudo chown -R bitnami:bitnami ../
 echo 'ownership changed'
 
 ## Set up node
 echo 'init'
-#npm i 
+npm i 
 echo 'build ng'
 #npm rebuild node-sass --force
-#/opt/bitnami/nodejs/bin/ng build -prod 2>&1 | tee outfile
+/opt/bitnami/nodejs/bin/ng build -prod 2>&1 | tee outfile
 
 echo 'end'
 
