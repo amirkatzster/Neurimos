ff3511008ef4ddc75a3d717a17dbe1952107821f
Amir Katz <amirkatzster@gmail.com>
2017-11-21 01:34:28 +0200

    packages...

--- scripts/post.sh
+++ scripts/post.sh
@@ -3,23 +3,25 @@
 set -e
 
 echo 'starting to run post script'
-## ownership
 whoami
-#chown root:root /usr/bin/sudo && chmod 4755 /usr/bin/sudo
-#chown root:root /usr/lib/sudo/sudoers.so && chmod 4755 /usr/lib/sudo/sudoers.so; chown root:root /etc/sudoers; chown root:root /etc/sudoers;
-#chown root:root /etc/sudoers.d && chmod 4755 /etc/sudoers.d; chown root:root /var/lib/sudo; chown root:root /var/lib/sudo;
-#chown -R root:root /etc 
 
+#global installations
+echo 'global installs'
+npm install -g @angular/cli
+npm install -g typescript@'>=2.1.0 <2.4.0' 
+
+echo 'ownership change'
 cd ~/www
 sudo chown -R bitnami:bitnami ../
-echo 'ownership changed'
+
 
 ## Set up node
 echo 'init'
-npm i 
+npm i | tee outfile
+
 echo 'build ng'
-#npm rebuild node-sass --force
-/opt/bitnami/nodejs/bin/ng build -prod 2>&1 | tee outfile
+#/opt/bitnami/nodejs/bin/ng build -prod 2>&1 | tee outfile
+npm run prod 2>&1 | tee -a outfile
 
 echo 'end'
 
