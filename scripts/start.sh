c89dcb8a1c08af206ee9fc45c0315c68f177ad8c
Amir Katz <amirkatzster@gmail.com>
2017-11-22 01:53:18 +0200

    ..

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,7 +2,10 @@
 
 set -e
 
-
+cd ~/www
 echo 'build ng'
-/opt/bitnami/nodejs/bin/ng build -prod 2>&1 | tee outfile
+
+/opt/bitnami/nodejs/bin/npm run prod 2>&1 | tee outfile
+
+#/opt/bitnami/nodejs/bin/ng build -prod 2>&1 | tee outfile
 #/opt/bitnami/nodejs/bin/npm run prod > /dev/null 2> /dev/null < /dev/null & # 2>&1 | tee -a outfile
