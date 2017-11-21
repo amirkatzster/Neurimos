79f2d318e81317113260d9ae72df800dafc4a171
Amir Katz <amirkatzster@gmail.com>
2017-11-22 01:18:53 +0200

    std redirect

--- scripts/post.sh
+++ scripts/post.sh
@@ -20,7 +20,7 @@ echo 'init'
 
 echo 'build ng'
 #/opt/bitnami/nodejs/bin/ng build -prod 2>&1 | tee outfile
-nohup /opt/bitnami/nodejs/bin/npm run prod & # 2>&1 | tee -a outfile
+/opt/bitnami/nodejs/bin/npm run prod > /dev/null 2> /dev/null < /dev/null & # 2>&1 | tee -a outfile
 
 
 echo 'end'
