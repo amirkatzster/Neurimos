45da63e615d575be8482855ffc4f77dccc8d339b
Amir Katz <amirkatzster@gmail.com>
2017-11-23 02:43:31 +0200

    ..

--- scripts/pre.sh
+++ scripts/pre.sh
@@ -4,7 +4,7 @@ set +e
 
 echo 'kill all node instances'
 NODE_PROCESS=$(ps -ef | grep "node")
-if [ -n "$NODE_PROCESS"]
+if [ -n "$NODE_PROCESS"]; then
    $NODE_PROCESS | awk '{print $2}' | xargs kill -9
 fi
 
