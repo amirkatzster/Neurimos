51db1c0e46f4842dbfee788d1f7d0a64bca02a8c
Amir Katz <amirkatzster@gmail.com>
2018-01-30 00:20:22 +0200

    ...

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,10 +8,13 @@ yum -y update
 #yum -y install default-jre ImageMagick
 
 # add nodejs to yum
-curl --silent --location https://rpm.nodesource.com/setup_9.x | bash -
-yum -y install nodejs #default-jre ImageMagick
-npm install npm@latest -g
+# curl --silent --location https://rpm.nodesource.com/setup_9.x | bash -
+# yum -y install nodejs #default-jre ImageMagick
 
+sudo npm cache clean -f
+sudo npm install -g n
+sudo n stable
+npm install npm@latest -g
 set +e
 npm install -g concurrently
 npm install -g @angular/cli
