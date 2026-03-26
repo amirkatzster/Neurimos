fee48149f9d6440f3379c9d01b1252a7fe84a738
Amir Katz <amirkatzster@gmail.com>
2026-03-26 02:11:55 +0200

    logo

--- scripts/bootstrapEC2.sh
+++ scripts/bootstrapEC2.sh
@@ -12,10 +12,23 @@ sudo ./install auto
 sudo service codedeploy-agent status
 
 # add nodejs to yum
-curl --silent --location https://rpm.nodesource.com/setup_9.x | bash -
-yum -y install nodejs
+# Using nvm (recommended)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+source ~/.bashrc
+nvm install 20
+nvm use 20
+nvm alias default 20
+npm install -g pm2
 
-sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
-sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 4000
+
+sudo dnf install -y nftables
+sudo nft add table ip nat
+sudo nft add chain ip nat prerouting { type nat hook prerouting priority 0 \; }
+sudo nft add rule ip nat prerouting tcp dport 80 redirect to :3000
+sudo nft add rule ip nat prerouting tcp dport 443 redirect to :4000
+
+#old...
+# sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 3000
+# sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 4000
 
 ## DONT FORGET TO COPY THE CONFIG FILES
