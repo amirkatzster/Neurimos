e868acf014d5fec93b57476742c4324cd4d80041
Amir Katz <amirkatzster@gmail.com>
2026-03-28 02:30:53 +0300

    focus

--- client/app/routing.module.ts
+++ client/app/routing.module.ts
@@ -46,7 +46,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, { scrollPositionRestoration: 'top' })
   ],
   exports: [RouterModule]
 })
